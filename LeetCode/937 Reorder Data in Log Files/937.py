7d2ae8196fd7f86ff3d0c1094ec9403ce3229965
krta2 <krta22@gmail.com>
2021-07-29 17:06:17 +0900

    Update LeetCode 937

--- LeetCode/937 Reorder Data in Log Files/937.py
+++ LeetCode/937 Reorder Data in Log Files/937.py
@@ -1,17 +1,4 @@
 from typing import *
-from functools import cmp_to_key
-
-
-def custom_cmp(a, b):
-    if a['content'] < b['content']:
-        return -1
-    elif a['content'] == b['content']:
-        if a['identifier'] < b['identifier']:
-            return -1
-        else:
-            return 1
-    else:
-        return 1
 
 
 class Solution:
@@ -19,27 +6,11 @@ class Solution:
         letter_logs = []
         digit_logs = []
         for log in logs:
-            first_space_position = log.find(' ')
-            item = {
-                'identifier': log[:first_space_position],
-                'content': log[first_space_position + 1:],
-            }
-
-            if log[first_space_position + 1].isalpha():
-                letter_logs.append(item)
+            if log.split()[1].isalpha():
+                letter_logs.append(log)
             else:
-                digit_logs.append(item)
+                digit_logs.append(log)
 
-        # Sorting two times is faster than custom sort
-        # letter_logs.sort(cmp_to_key(custom_cmp))
+        letter_logs.sort(key=lambda x: (x.split()[1:], x.split()[0]))
 
-        letter_logs.sort(key=lambda x: x['identifier'])
-        letter_logs.sort(key=lambda x: x['content'])
-
-        result = []
-        for letter_log in letter_logs:
-            result.append(letter_log['identifier'] + ' ' + letter_log['content'])
-        for digit_log in digit_logs:
-            result.append(digit_log['identifier'] + ' ' + digit_log['content'])
-
-        return result
+        return letter_logs + digit_logs
